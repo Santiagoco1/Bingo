083ecd332e6be2c40a23713e9dc4dfaec9e67c61
Santiago Co <cosantiago2019@gmail.com>
2020-07-03 12:59:14 -0300

    Terminada tarea 6

--- render-carton.py
+++ render-carton.py
@@ -1,6 +1,9 @@
 from jinja2 import Template
+from src.bingo import comprobarCarton
 
 template = Template(open('plantilla.j2').read())
 
-print(template.render(usuario="Luis", color="rojo"))
-print(template.render(usuario="Dagos", color="azul"))
+file = open("carton.html", "w")
+file.write(template.render(carton = comprobarCarton()))
+file.close()
+print("Generado \"carton.html\".")
